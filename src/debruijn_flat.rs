467cb06870d2c2b6cd02e528c50fa008fadbd941
Aaron Kofsky <aaronko@umich.edu>
2025-09-14 21:39:36 -0400

    Improve graphviz output for FlatRoot.

--- src/debruijn_flat.rs
+++ src/debruijn_flat.rs
@@ -207,6 +207,16 @@ impl FlatRoot {
     }
 
     pub fn to_graph(&self) -> String {
+        fn get_garbage_array(root: &FlatRoot) -> Vec<bool> {
+            let mut is_garbage = vec![true; root.backing.len()];
+            VisitOrder::LEFT_OUTERMOST.preorder_walk_2(root, |_, term, _| {
+                is_garbage[term.term.0] = false;
+                ControlFlow::Continue::<()>(())
+            });
+
+            is_garbage
+        }
+
         fn to_node_label(term: DebruijnNode) -> String {
             match term {
                 DebruijnNode::Index(index) => format!("idx_{index}"),
@@ -215,21 +225,40 @@ impl FlatRoot {
             }
         }
 
+        let is_garbage = get_garbage_array(self);
+
         let mut output = vec![];
         output.push(format!("strict digraph G {{"));
 
-        let root = format!("{} [color = red];", self.root,);
-        output.push(root);
-
         for (index, node) in self.backing.iter().enumerate() {
-            let node_text = format!("{} [label = \"{} @ {index}\"]", index, to_node_label(*node));
+            let is_garbage = is_garbage[index];
+            let node_color = if is_garbage {
+                "grey"
+            } else if index == self.root.0 {
+                "red"
+            } else {
+                "black"
+            };
+            let label = format!("{} @ {index}", to_node_label(*node));
+            let attribs = if is_garbage {
+                format!(
+                    "label=\"{label}\" color={node_color} fontcolor={node_color} constraint=false"
+                )
+            } else {
+                format!("label=\"{label}\" color={node_color} fontcolor={node_color}")
+            };
+            let node_text = format!("{index} [{attribs}]");
             output.push(node_text);
+
+            let edge_color = if is_garbage { "grey" } else { "black" };
             match node {
-                DebruijnNode::Index(_) => output.push(format!("")),
-                DebruijnNode::Abstraction { body, .. } => output.push(format!("{index} -> {body}")),
+                DebruijnNode::Index(_) => (),
+                DebruijnNode::Abstraction { body, .. } => {
+                    output.push(format!("{index} -> {body} [color={edge_color}]"))
+                }
                 DebruijnNode::Application { func, arg } => {
-                    output.push(format!("{index} -> {func}"));
-                    output.push(format!("{index} -> {arg}"));
+                    output.push(format!("{index} -> {func} [color={edge_color}]"));
+                    output.push(format!("{index} -> {arg} [color={edge_color}]"));
                 }
             }
         }
