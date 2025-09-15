3c8b183b06858edf115bdc0aaa40825f7f6ee799
Aaron Kofsky <aaronko@umich.edu>
2025-09-14 22:38:56 -0400

    Make graph vizualization prettier.

--- src/bin/graphviz2.rs
+++ src/bin/graphviz2.rs
@@ -1,18 +1,31 @@
+#![feature(iter_intersperse)]
+
 use std::{ops::ControlFlow, str::FromStr};
 
 use clap::Parser;
 use lambda_beaver::{
     debruijn::Debruijn,
-    debruijn_flat::{DebruijnNode, FlatRoot},
+    debruijn_flat::{DebruijnNode, FlatRoot, TermIndex},
     parse,
     reduce::Reducer,
     treewalk::VisitOrder,
 };
 
-fn get_garbage_array(root: &FlatRoot) -> Vec<bool> {
-    let mut is_garbage = vec![true; root.backing.len()];
-    VisitOrder::LEFT_OUTERMOST.preorder_walk_2(root, |_, term, _| {
-        is_garbage[term.term.0] = false;
+fn get_garbage_array(root: &FlatRoot) -> Vec<(bool, Option<TermIndex>)> {
+    let mut is_garbage = vec![(true, None); root.backing.len()];
+    VisitOrder::LEFT_OUTERMOST.preorder_walk_2(root, |_, term, parent_chain| {
+        let abs_bound = match root[term.term] {
+            DebruijnNode::Index(index) => {
+                if index <= parent_chain.len() {
+                    Some(parent_chain[parent_chain.len() - index])
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        };
+
+        is_garbage[term.term.0] = (false, abs_bound);
         ControlFlow::Continue::<()>(())
     });
 
@@ -21,50 +34,80 @@ fn get_garbage_array(root: &FlatRoot) -> Vec<bool> {
 
 fn to_node_label(term: DebruijnNode) -> String {
     match term {
-        DebruijnNode::Index(index) => format!("idx_{index}"),
-        DebruijnNode::Abstraction { usage, .. } => format!("abs, usage = {usage}"),
+        DebruijnNode::Index(index) => format!("idx: {index}"),
+        DebruijnNode::Abstraction { usage, .. } => format!("abs\nusage = {usage}"),
         DebruijnNode::Application { .. } => format!("app"),
     }
 }
+
+const GARBAGE_COLOR: &str = "lightgrey";
+const ROOT_COLOR: &str = "red";
+const NORMAL_COLOR: &str = "black";
 
 pub fn to_graph(root: &FlatRoot) -> String {
     let is_garbage = get_garbage_array(root);
 
     let mut output = vec![];
-    output.push(format!("strict digraph G {{"));
-
+    output.push(format!("digraph G {{"));
+    output.push("node [shape=box]".to_string());
     for (index, node) in root.backing.iter().enumerate() {
-        let is_garbage = is_garbage[index];
+        let (is_garbage, index_bound) = is_garbage[index];
         let node_color = if is_garbage {
-            "grey"
+            GARBAGE_COLOR
         } else if index == root.root.0 {
-            "red"
+            ROOT_COLOR
         } else {
-            "black"
+            NORMAL_COLOR
         };
-        let label = format!("{} @ {index}", to_node_label(*node));
-        let attribs = if is_garbage {
-            format!("label=\"{label}\" color={node_color} fontcolor={node_color} constraint=false")
-        } else {
-            format!("label=\"{label}\" color={node_color} fontcolor={node_color}")
-        };
+
+        let attribs = vec![
+            ("label", to_node_label(*node)),
+            ("xlabel", index.to_string()),
+            ("color", node_color.to_string()),
+            ("fontcolor", node_color.to_string()),
+        ];
+
+        let attribs = bake_attribs(&attribs);
+
         let node_text = format!("{index} [{attribs}]");
         output.push(node_text);
 
-        let edge_color = if is_garbage { "grey" } else { "black" };
+        let edge_color = if is_garbage {
+            GARBAGE_COLOR
+        } else {
+            NORMAL_COLOR
+        };
+
+        let edge_attribs = format!("color={edge_color}");
         match node {
-            DebruijnNode::Index(_) => (),
+            DebruijnNode::Index(_) => {
+                if let Some(abs_bound) = index_bound {
+                    output.push(format!(
+                        "{index} -> {abs_bound} [color=cyan, style=dashed, constraint=false]"
+                    ))
+                }
+            }
             DebruijnNode::Abstraction { body, .. } => {
-                output.push(format!("{index} -> {body} [color={edge_color}]"))
+                output.push(format!("{index} -> {body} [{edge_attribs}]"))
             }
             DebruijnNode::Application { func, arg } => {
-                output.push(format!("{index} -> {func} [color={edge_color}]"));
-                output.push(format!("{index} -> {arg} [color={edge_color}]"));
+                output.push(format!("{index} -> {func} [{edge_attribs}]"));
+                output.push(format!(
+                    "{index} -> {arg} [{edge_attribs} arrowhead=onormal]"
+                ));
             }
-        }
+        };
     }
     output.push(format!("}}"));
     output.join("\n")
+}
+
+fn bake_attribs(attribs: &[(&str, String)]) -> String {
+    attribs
+        .iter()
+        .map(|(name, value)| format!("{name}=\"{}\"", value))
+        .intersperse(" ".to_string())
+        .collect()
 }
 
 #[derive(Parser, Debug)]
